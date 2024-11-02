f58d947ee39143b03e8a5edc7f1851a25b16694e
Thomas Metten <tom.metten@acagroup.be>
2024-11-02 22:23:41 +0100

    feat: add main menu

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,25 @@
 extends Node
 
+@onready var combat_scene = preload("res://scenes/Combat.tscn")
+@onready var menu_scene = preload("res://scenes/MainMenu.tscn")
 
-func combat_won() -> void:
+var current_scene: Node = null
+
+func _ready():
+	show_main_menu()
+
+func show_main_menu():
+	if current_scene:
+		current_scene.queue_free()
+	current_scene = menu_scene.instantiate()
+	add_child(current_scene)
+
+func start_combat():
+	if current_scene:
+		current_scene.queue_free()
+	current_scene = combat_scene.instantiate()
+	add_child(current_scene)
+
+func combat_won():
 	print("Combat won")
+	show_main_menu()
---
+++ scenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	$CenterContainer/VBoxContainer/StartButton.pressed.connect(_on_start_pressed)
+	$CenterContainer/VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+
+func _on_start_pressed():
+	get_node("/root/Main").start_combat()
+
+func _on_quit_pressed():
+	get_tree().quit()
