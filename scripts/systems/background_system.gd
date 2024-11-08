2cd2999a6d1a7774f705cb956297b2a058384b25
Thomas Metten <tom.metten@acagroup.be>
2024-11-08 20:34:55 +0100

    fix: resize background on non-matching resolutions
    Also clean up any previously set backgrounds

--- scripts/systems/background_system.gd
+++ scripts/systems/background_system.gd
@@ -7,6 +7,7 @@ const COMBAT_BG = preload("res://assets/backgrounds/combat_bg.png")
 var current_background: Control
 
 func setup_for_scene(scene_type: String) -> void:
+	get_children().all(remove_child)
 	print("Background System: Setting up background for ", scene_type)
 	
 	var background: Control
@@ -22,17 +23,10 @@ func setup_for_scene(scene_type: String) -> void:
 			background = ColorRect.new()
 			background.color = Color.DARK_TURQUOISE
 	
-	if background is TextureRect:
-		background.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
-	
-	background.set_anchors_preset(Control.PRESET_FULL_RECT)
-	background.z_index = -1
-	background.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	
-	add_child(background)
-	current_background = background
+	configure_and_set(background)
 	
 func setup_background(backgroundResource: Resource) -> void:
+	get_children().all(remove_child)
 	print("Background System: Setting up background for encounter")
 	
 	var background: Control
@@ -45,8 +39,13 @@ func setup_background(backgroundResource: Resource) -> void:
 		background = ColorRect.new()
 		background.color = Color.DARK_TURQUOISE
 	
+	configure_and_set(background)
+
+func configure_and_set(background: Control) -> void:
 	if background is TextureRect:
 		background.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+		background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+
 	
 	background.set_anchors_preset(Control.PRESET_FULL_RECT)
 	background.z_index = -1
