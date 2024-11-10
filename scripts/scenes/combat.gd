22b01730362f5089ce3d02e37289b71f8f86f7ef
Thomas Metten <tom.metten@acagroup.be>
2024-11-10 13:41:03 +0100

    feat: add health / block / buff bar for player and enemies
    Also added a placeholder texture for the player and introduced a
    separate scene for the player.
    
    Enemies now have a base scene that contains the script and health bar.

--- scripts/scenes/combat.gd
+++ scripts/scenes/combat.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 @onready var combat_manager = $CombatManager
 @onready var hud = $UI/HUD
+@onready var player = $Player
+@onready var enemies = $EnemiesContainer
 
 func _ready() -> void:
 	# Set up HUD to fill screen
@@ -32,15 +34,23 @@ func _ready() -> void:
 	energy_container.anchor_left = 1.0
 	energy_container.anchor_right = 1.0
 	
+	# Position player
+	player.position.x = get_viewport_rect().size.x / 8 * 2
+	player.position.y = get_viewport_rect().size.y  - 1000
+	
+		# Position player
+	enemies.position.x = get_viewport_rect().size.x / 8 * 5
+	enemies.position.y = get_viewport_rect().size.y  - 1000
+	
 	# We no longer need this connection since the combat manager connects directly to the button
 	# $UI/HUD/EndTurnMarginContainer/EndTurnButton.pressed.connect(_on_end_turn_pressed)
 	
 	# Connect to enemy death or victory condition
-	$EnemiesContainer.child_exiting_tree.connect(_check_combat_state)
+	enemies.child_exiting_tree.connect(_check_combat_state)
 
 func _check_combat_state(_node) -> void:
 	# If all enemies are dead
-	if $EnemiesContainer.get_child_count() <= 1:  # <= 1 because the signal fires before the node is fully removed
+	if enemies.get_child_count() <= 1:  # <= 1 because the signal fires before the node is fully removed
 		_on_combat_won()
 
 func _on_combat_won() -> void:
